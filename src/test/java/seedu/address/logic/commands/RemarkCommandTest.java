5b7d65f6f7b27524c70266a29fbfca23577bb72b
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2024-10-02 02:00:52 +0800

    Add new line at end of file

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -112,5 +112,4 @@ public class RemarkCommandTest {
         assertFalse(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON,
                 new Remark(VALID_REMARK_BOB))));
     }
-    
 }
